d54d8ac74fd5aede999ec5329b6690bba085794b
joseblack <jmartinez240@gmail.com>
2024-02-22 15:46:47 -0500

    first commit

---
+++ src/components/Resume/Index.vue
@@ -0,0 +1,92 @@
+<template>
+    <main>
+        <p>{{ labelVisual }}</p>
+        <h1 :class="{ 
+                'red': isNegative, 
+                'green': !isNegative
+                }">{{ amountCurrency }}</h1>
+        <div class="graphic">
+            <slot name="graphic"></slot>
+        </div>
+        <div class="action">
+            <slot name="action"></slot>
+        </div>
+    </main>
+</template>
+
+<script>
+const currencyFormatter = new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+});
+
+export default {
+    props: {
+        totalLabel : {
+            type: String,
+        },
+        label: {
+            type: String,
+        },
+        totalAmount: {
+            type: Number,
+        },
+        amount: {
+            type: Number,
+            default: null,
+        }
+    },
+    computed: {
+        amountVisual() {
+            return this.amount !== null ? this.amount : this.totalAmount;
+        },
+        labelVisual() {
+            return this.label !== null ? this.label : this.totalLabel
+        },
+        amountCurrency() {
+            return currencyFormatter.format(this.amountVisual);
+        },
+        isNegative() {
+            return this.amount !== null ? this.amount < 0 : this.totalAmount < 0;
+        }
+    },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+
+.red {
+    color: var(--brand-red);
+}
+
+.green {
+    color: var(--brand-green);
+}
+</style>
